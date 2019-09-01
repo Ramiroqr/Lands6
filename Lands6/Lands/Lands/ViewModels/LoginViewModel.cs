b3a5150fa19b9dc8da3a82d07a463268ba47eef2
Ramiroqr <ramiroquisper@gmail.com>
2019-09-01 12:27:58 -0500

    LoginViewModel

---
+++ Lands6/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+
+        #endregion
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+        
+    }
+}

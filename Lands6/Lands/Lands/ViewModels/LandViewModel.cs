9d6ed077a2264ece27eb61f83d63a90dbe9e1951
Ramiro Quispe Rampas <ramiroquisper@gmail.com>
2019-10-22 18:30:00 -0500

    LandPage

---
+++ Lands6/Lands/Lands/ViewModels/LandViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Lands.ViewModels
+{
+    using Lands.Models;
+    class LandViewModel
+    {
+        #region Properties
+        public Land Land
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        } 
+        #endregion
+    }
+}

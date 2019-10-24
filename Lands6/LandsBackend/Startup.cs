decef4bb984ffd03fd69e1e6d665bfbeebe99203
Ramiro Quispe Rampas <ramiroquisper@gmail.com>
2019-10-24 10:00:15 -0500

    Backend

---
+++ Lands6/LandsBackend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LandsBackend.Startup))]
+namespace LandsBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
